83e9336f5da638cbeb37c0cc9754ab60b368eced
pokhrelworks <work.ankitpokhrel@gmail.com>
2024-11-19 22:06:12 +0545

    resolved signup error

--- backend/Routes/SecretPage.js
+++ backend/Routes/SecretPage.js
@@ -2,6 +2,7 @@ const ensureAuthenticated = require("../Middlewares/Auth");
 const router = require("express").Router();
 
 router.get("/", ensureAuthenticated, (req, res) => {
+    console.log(req.user);
   res.status(200).json({
     message: "This is a secret page",
   });
